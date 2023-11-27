022dd0c2aff0a33a543f408da17f02be438b3382
Maria <79389256+marythedev@users.noreply.github.com>
2023-11-27 17:41:24 -0500

    updated health check route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const { version, author } = require('../../package.json');
 const { authenticate } = require('../auth');
 const { createSuccessResponse } = require('../response');
 const router = express.Router();
+const { hostname } = require('os');
 
 //Expose all of API routes on /v1/* to include an API version.
 router.use(`/v1`, authenticate(), require('./api'));
@@ -12,14 +13,14 @@ router.get('/', (req, res) => {
   //makes sure the response isn't cached
   //https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
   res.setHeader('Cache-Control', 'no-cache');
-
-  let data = {
-    author,
-    githubUrl: 'https://github.com/marythedev/fragments',
-    version,
-  };
-
-  res.status(200).json(createSuccessResponse(data));
+  res.status(200).json(
+    createSuccessResponse({
+      author,
+      githubUrl: 'https://github.com/marythedev/fragments',
+      version,
+      hostname: hostname(),
+    })
+  );
 });
 
 module.exports = router;
